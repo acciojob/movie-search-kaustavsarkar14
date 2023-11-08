3710bce6ba98e4943757f409fe0a425cabf1f007
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-09 00:35:28 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [error, setError] = useState(null)
+
+
+  function searchMovies() {
+    fetch(`https://www.omdbapi.com/?apikey=bd2e9ae2&s=${searchTerm}`)
+      .then(data => data.json())
+      .then(data => {
+        if (data.Error) setError(data.Error)
+        else {
+          setMovies(data.Search)
+          setError(null)
+        }
+      })
+  }
+  console.log(movies)
+  console.log(error)
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+      <button onClick={searchMovies} >Search</button>
+      {
+        error ?
+          <h1 className="error" >Invalid movie name</h1>
+          :
+          <ul>
+            {
+              movies.map(movie => {
+                return (
+                  <li key={movie.imdbID} >
+                    <h2>{movie.Title} {movie.Year}</h2>
+                    <img src={movie.Poster} alt="" />
+                  </li>
+                )
+              })
+            }
+          </ul>
+      }
     </div>
   )
 }
